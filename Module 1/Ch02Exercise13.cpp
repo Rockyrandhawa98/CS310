53f50ff49a12d93321eb8a19e94e0203d392b728
Rocky Randhawa <rockyrandhawa98@gmail.com>
2024-08-26 16:57:55 -0400

    Changed prompts in cin and cout lines to better suit the specifications of the assignment

--- Module 1/Ch02Exercise13.cpp
+++ Module 1/Ch02Exercise13.cpp
@@ -7,9 +7,9 @@ int main() {
 
     std::cout << "What is the initial price of the item? " << std::endl;
     std::cin >> price_of_item;
-    std::cout << "What is the store's mark-up rate? " << std::endl;
+    std::cout << "What is the store's mark-up rate? \n(Please enter as a decimal; For instance, if the mark-up is 15%, enter 0.15.) " << std::endl;
     std::cin >> mark_up;
-    std::cout << "What is the sale's tax? " << std::endl;
+    std::cout << "What is the sales tax? \n(Please enter as a decimal; For instance, if the sales tax is 15%, enter 0.15.) " << std::endl;
     std::cin >> sales_tax_rate;
 
     double store_selling_price = price_of_item + (price_of_item * mark_up);
@@ -17,11 +17,10 @@ int main() {
     double final_price = store_selling_price + sales_tax;
 
     std::cout << "The initial price of the item is $" << price_of_item << "." << std::endl;
-    std::cout << "The mark up is " << mark_up << "%." << std::endl;
+    std::cout << "The mark up is " << mark_up * 100 << "%." << std::endl;
     std::cout << "The store is selling the item at $" << store_selling_price << "." << std::endl;
-    std::cout << "The sale's tax rate is " << sales_tax_rate << "%." << std::endl;
-    std::cout << "The sale's tax is $" << sales_tax << "." << std::endl;
+    std::cout << "The sales tax rate is " << sales_tax_rate * 100 << "%." << std::endl;
+    std::cout << "The sales tax is $" << sales_tax << "." << std::endl;
     std::cout << "The final sale price is $" << final_price << "." << std::endl;
-
     return 0;
 }
