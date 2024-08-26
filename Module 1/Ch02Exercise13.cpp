87c67d5459e71cd8ffd967f39ef3fe33c32b7354
Rocky Randhawa <rockyrandhawa98@gmail.com>
2024-08-26 16:51:04 -0400

    Updated M1 assignment to take in user input for the original price of the item sold, markup, and sales tax rate.

--- Module 1/Ch02Exercise13.cpp
+++ Module 1/Ch02Exercise13.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 
-int price_of_item = 15;
-double mark_up = .15;
-double store_selling_price = price_of_item + (price_of_item * mark_up);
-double sales_tax_rate = .08;
-double sales_tax = store_selling_price * sales_tax_rate;
-double final_price = store_selling_price + sales_tax;
-
 int main() {
-    
+    float price_of_item;
+    double mark_up;
+    double sales_tax_rate;
+
+    std::cout << "What is the initial price of the item? " << std::endl;
+    std::cin >> price_of_item;
+    std::cout << "What is the store's mark-up rate? " << std::endl;
+    std::cin >> mark_up;
+    std::cout << "What is the sale's tax? " << std::endl;
+    std::cin >> sales_tax_rate;
+
+    double store_selling_price = price_of_item + (price_of_item * mark_up);
+    double sales_tax = store_selling_price * sales_tax_rate;
+    double final_price = store_selling_price + sales_tax;
+
     std::cout << "The initial price of the item is $" << price_of_item << "." << std::endl;
     std::cout << "The mark up is " << mark_up << "%." << std::endl;
     std::cout << "The store is selling the item at $" << store_selling_price << "." << std::endl;
