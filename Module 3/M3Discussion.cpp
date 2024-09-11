d20981fa50a2f01727090da46b2f0240b627f5a7
Rocky Randhawa <rockyrandhawa98@gmail.com>
2024-09-11 10:03:11 -0400

    Added M3 Discussion

--- Module 3/M3Discussion.cpp
+++ Module 3/M3Discussion.cpp
@@ -1,7 +1,7 @@
 /* 
 This is a simplified version of the "FizzBuzz" problem,
-which takes in a number and outputs a word based on its divisors: 3 and 5.
-If there is no valid divisor, it outputs the number itself.
+which takes in a number and outputs a word if it is divisible by 3 and/or 5.
+If the number is not divisible by 3 or 5, it outputs the number itself.
 */
 
 #include <iostream>
